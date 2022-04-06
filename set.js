befef8107f314030e66b2c4e1017909fe09b9a01
NaniMadhu94 <102646505+NaniMadhu94@users.noreply.github.com>
2022-04-06 11:03:23 +0530

    Update set.js

--- set.js
+++ set.js
@@ -1,10 +1,10 @@
 var foot = {
     kick: function () {
         this.yelp = "Ouch!";
+        /* pass  setImmediate function and as this.yelp is undefined use .bindthis to print the value using console*/
         setImmediate(function () {
             console.log(this.yelp);
         }.bind(this));
     }
 };
 foot.kick();
-
