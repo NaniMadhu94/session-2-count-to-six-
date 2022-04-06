c3a28ab61637fcf8f5fb5bdebea7dfaf1af5ab32
NaniMadhu94 <102646505+NaniMadhu94@users.noreply.github.com>
2022-04-06 11:01:32 +0530

    Update domenic.js

--- domenic.js
+++ domenic.js
@@ -1,3 +1,6 @@
+/* Provide the frist argv to the prg*/
 const inputString =  process.argv[2];
+
+/*  print  an output of a 2 line msg using console*/
 console.log(`Hello, ${inputString}!
 Your name lowercased is \"${inputString.toLowerCase()}\".`);
