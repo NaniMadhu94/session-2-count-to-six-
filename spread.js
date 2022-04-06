be722b93678f1cfecc41398e12cadc23691fe00c
NaniMadhu94 <102646505+NaniMadhu94@users.noreply.github.com>
2022-04-06 11:05:15 +0530

    Update spread.js

--- spread.js
+++ spread.js
@@ -1,3 +1,7 @@
+/* provoide a varialble containg argvs to the prg */ 
 let input=process.argv.slice(2);
-let min=Math.min(...input)
+
+/* use and ES6 operator to find the min value */
+ 
+min=Math.min(...input)
 console.log(`The minimum of [${input}] is ${min}`);
