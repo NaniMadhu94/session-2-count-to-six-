fc43d2a302d30012456f68680c4fdd6c6243c5e2
NaniMadhu94 <102646505+NaniMadhu94@users.noreply.github.com>
2022-04-06 10:59:20 +0530

    Update arrow.js

--- arrow.js
+++ arrow.js
@@ -1,3 +1,9 @@
 const inputs=process.argv.slice(2);
 let result=inputs.map((input)=>input[0]).join('');
 console.log(`[${inputs}] becomes \"${result}"`);
+/* provide var with the argvs*/
+const inputs=process.argv.slice(2);
+
+/* use arrow function '=>' */
+let result=inputs.map((input)=>input[0]).join('');
+console.log(`[${inputs}] becomes \"${result}"`);
